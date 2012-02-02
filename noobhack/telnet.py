0f5a7f1147b300eefdbc657d9cdbe4b0be7a2be1
miton <mitongasukiyo@gmail.com>
2012-02-02 01:36:46 -0500

    %s/send/sendall/

--- noobhack/telnet.py
+++ noobhack/telnet.py
@@ -16,7 +16,7 @@ class Telnet:
 
     def write(self, buf):
         """ Proxy input to the telnet process' stdin. """
-        self.conn.get_socket().send(buf)
+        self.conn.get_socket().sendall(buf)
 
     def read(self):
         """ 
@@ -46,38 +46,38 @@ class Telnet:
         """ Configure our telnet options. This is magic. Don't touch it. """
 
         if command == telnetlib.DO and option == "\x18":
-            # Promise we'll send a terminal type
-            socket.send("%s%s\x18" % (telnetlib.IAC, telnetlib.WILL))
+            # Promise we'll sendall a terminal type
+            socket.sendall("%s%s\x18" % (telnetlib.IAC, telnetlib.WILL))
         elif command == telnetlib.DO and option == "\x01":
             # Pinky swear we'll echo
-            socket.send("%s%s\x01" % (telnetlib.IAC, telnetlib.WILL))
+            socket.sendall("%s%s\x01" % (telnetlib.IAC, telnetlib.WILL))
         elif command == telnetlib.DO and option == "\x1f":
-            # And we should probably tell the server we will send our window
+            # And we should probably tell the server we will sendall our window
             # size
             logging.debug("callback on option NAWS")
-            socket.send("%s%s\x1f" % (telnetlib.IAC, telnetlib.WILL))
-            #socket.send("%s%s\x1f\x00\x96\x00\x1E%s%s" % (telnetlib.IAC, telnetlib.SB, telnetlib.IAC, telnetlib.SE))
+            socket.sendall("%s%s\x1f" % (telnetlib.IAC, telnetlib.WILL))
+            #socket.sendall("%s%s\x1f\x00\x96\x00\x1E%s%s" % (telnetlib.IAC, telnetlib.SB, telnetlib.IAC, telnetlib.SE))
         elif command == telnetlib.DO and option == "\x20":
-            # Tell the server to sod off, we won't send the terminal speed
-            socket.send("%s%s\x20" % (telnetlib.IAC, telnetlib.WONT))
+            # Tell the server to sod off, we won't sendall the terminal speed
+            socket.sendall("%s%s\x20" % (telnetlib.IAC, telnetlib.WONT))
         elif command == telnetlib.DO and option == "\x23":
-            # Tell the server to sod off, we won't send an x-display terminal
-            socket.send("%s%s\x23" % (telnetlib.IAC, telnetlib.WONT))
+            # Tell the server to sod off, we won't sendall an x-display terminal
+            socket.sendall("%s%s\x23" % (telnetlib.IAC, telnetlib.WONT))
         elif command == telnetlib.DO and option == "\x27":
-            # We will send the environment, though, since it might have nethack
+            # We will sendall the environment, though, since it might have nethack
             # specific options in it.
-            socket.send("%s%s\x27" % (telnetlib.IAC, telnetlib.WILL))
+            socket.sendall("%s%s\x27" % (telnetlib.IAC, telnetlib.WILL))
         elif self.conn.rawq.startswith("\xff\xfa\x27\x01\xff\xf0\xff\xfa"):
             # We're being asked for the environment settings that we promised
             # earlier
-            socket.send("%s%s\x27\x00%s%s%s" %
+            socket.sendall("%s%s\x27\x00%s%s%s" %
                         (telnetlib.IAC,
                          telnetlib.SB,
                          '\x00"NETHACKOPTIONS"\x01"%s"' % os.environ.get("NETHACKOPTIONS", ""),
                          telnetlib.IAC,
                          telnetlib.SE))
             # We're being asked for the terminal type that we promised earlier
-            socket.send("%s%s\x18\x00%s%s%s" % 
+            socket.sendall("%s%s\x18\x00%s%s%s" % 
                         (telnetlib.IAC,
                          telnetlib.SB,
                          "xterm-color",
