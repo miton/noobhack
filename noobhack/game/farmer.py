90b03ba0315f0619d9c637278ea90aaeff9ddc67
miton <mitongasukiyo@gmail.com>
2012-02-08 06:45:25 -0500

    update keepinv

--- noobhack/game/farmer.py
+++ noobhack/game/farmer.py
@@ -38,7 +38,7 @@ class Farmer:
         self.named = True
         self.sac = False
         self.safe_pray = False
-        self.keep_inv = 'JjCiPwEvbtRyFSpMsBaAmoeXu'
+        self.keep_inv = 'JCiPwEvbtRysuFSghaAmoeX'
         self.unidentified_count = 0 #not likely to be accurate
         self.spell_menu = False
         self.found_spell = False
